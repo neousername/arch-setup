6c8cfaf387c6d2143cc5410ec0a90722deb6e8ff
neousername <vitaliyovich.oleg@gmail.com>
2026-06-06 17:48:44 +0200

    Wow

---
+++ .config/nvim/after/lsp/lua_ls.lua
@@ -0,0 +1,15 @@
+return {
+  settings = {
+    Lua = {
+      runtime = {
+        version = 'LuaJIT',
+      },
+      workspace = {
+        preloadFileSize = 10000,
+        library = {
+          vim.env.VIMRUNTIME,
+        },
+      },
+    },
+  },
+}
