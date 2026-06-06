7962df75579d9b70151b7ad16d58caa2260917fc
neousername <vitaliyovich.oleg@gmail.com>
2026-06-06 18:11:54 +0200

    Changes

--- .config/nvim/lua/config/settings.lua
+++ .config/nvim/lua/config/settings.lua
@@ -1,6 +1,6 @@
 vim.g.mapleader = " "
 vim.keymap.set("n", "<leader>pv", vim.cmd.Ex)
-vim.opt.number=true
+vim.opt.number = true
 vim.opt.cursorline = true
 vim.opt.shiftwidth = 2
 vim.opt.scrolloff = 100
@@ -20,4 +20,53 @@ vim.diagnostic.config({
       [severity.INFO] = "",
     }
   }
+})
+
+-- LSP Setup
+vim.lsp.enable({
+  "lua_ls",
+  "pylsp",
+})
+
+-- Global LSP Keybinds
+vim.api.nvim_create_autocmd('LspAttach', {
+  group = vim.api.nvim_create_augroup('my.lsp', {}),
+
+  callback = function(ev)
+    local client = assert(vim.lsp.get_client_by_id(ev.data.client_id))
+
+    vim.keymap.set('n', 'gd', vim.lsp.buf.definition, {
+      buffer = ev.buf,
+      desc = 'Go to definition',
+    })
+
+    vim.keymap.set('n', 'gD', vim.lsp.buf.declaration, {
+      buffer = ev.buf,
+      desc = 'Go to declaration',
+    })
+
+    vim.keymap.set('n', 'K', vim.lsp.buf.hover, {
+      buffer = ev.buf,
+      desc = 'Hover documentation',
+    })
+
+    if client:supports_method('textDocument/implementation', ev.buf) then
+      vim.keymap.set('n', 'gi', vim.lsp.buf.implementation, {
+        buffer = ev.buf,
+        desc = 'Go to implementation',
+      })
+    end
+
+    if client:supports_method('textDocument/formatting', ev.buf) then
+      vim.keymap.set('n', '<leader>lf', function()
+        vim.lsp.buf.format({
+          bufnr = ev.buf,
+          id = client.id,
+        })
+      end, {
+        buffer = ev.buf,
+        desc = 'Format buffer',
+      })
+    end
+  end,
 })
